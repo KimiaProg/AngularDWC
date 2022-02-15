6d451f2a957205b6bd290d9f8d44a40eb80d173d
kimia <kimia.ehsani.dolati.al@iespoligonosur.org>
2022-02-15 17:35:40 +0100

    componenets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AlumnadoDAWComponent } from './alumnado-daw/alumnado-daw.component';
 import { EmpleadoComponent } from './empleado/empleado.component';
+import { CocheComponent } from './coche/coche.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
 {path: 'empleado', component: EmpleadoComponent},
 {path: 'alumnado', component: AlumnadoDAWComponent},
+{path: 'home', component: HomeComponent},
+{path: 'contacto', component: ContactoComponent},
+{path: 'coche', component: CocheComponent},
 {path: '', component: EmpleadoComponent},
 {path: '**', component: EmpleadoComponent}
 ];
